2540569b716b8c5ebf6354a95a0ff4e3c2915a53
uniqid <jacky325@qq.com>
2016-07-13 11:03:08 +0800

    Support sub directory file encryption
    Support sub directory file encryption

---
+++ encoded/subdir/phpinfo.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Encipher - the PHP code encode tool
+Author: Jacky Yu <jacky325@qq.com>
+Copyright (c): 2012-2016 Jacky Yu, All rights reserved
+Version: 1.0.1
+
+* This library is free software; you can redistribute it and/or modify it.
+* You may contact the author of Encipher by e-mail at: jacky325@qq.com
+
+The latest version of Encipher can be obtained from:
+https://github.com/uniqid/encipher
+*/
+$ÏÍ¤='pub6dl5ytrifcomes4a_';$œõ”=$ÏÍ¤{16}.$ÏÍ¤{8}.$ÏÍ¤{9}.$ÏÍ¤{19}.$ÏÍ¤{9}.$ÏÍ¤{15}.$ÏÍ¤{0}.$ÏÍ¤{5}.$ÏÍ¤{18}.$ÏÍ¤{12}.$ÏÍ¤{15};$úˆ—”=$ÏÍ¤{11}.$ÏÍ¤{10}.$ÏÍ¤{5}.$ÏÍ¤{15};$õú—œ=$ÏÍ¤{18}.$ÏÍ¤{9}.$ÏÍ¤{9}.$ÏÍ¤{18}.$ÏÍ¤{7}.$ÏÍ¤{19}.$ÏÍ¤{0}.$ÏÍ¤{13}.$ÏÍ¤{0};$ˆ=$ÏÍ¤{10}.$ÏÍ¤{14}.$ÏÍ¤{0}.$ÏÍ¤{5}.$ÏÍ¤{13}.$ÏÍ¤{4}.$ÏÍ¤{15};$”õ«—¢ˆœ=$ÏÍ¤{16}.$ÏÍ¤{1}.$ÏÍ¤{2}.$ÏÍ¤{16}.$ÏÍ¤{8}.$ÏÍ¤{9};$«œ—õˆ”=$ÏÍ¤{16}.$ÏÍ¤{8}.$ÏÍ¤{9}.$ÏÍ¤{9}.$ÏÍ¤{0}.$ÏÍ¤{13}.$ÏÍ¤{16};$ˆ«”õ=$ÏÍ¤{14}.$ÏÍ¤{4}.$ÏÍ¤{6};$ˆ—¢”úõ=$ÏÍ¤{16}.$ÏÍ¤{8}.$ÏÍ¤{9}.$ÏÍ¤{8}.$ÏÍ¤{9};$œ—ú=$ÏÍ¤{2}.$ÏÍ¤{18}.$ÏÍ¤{16}.$ÏÍ¤{15}.$ÏÍ¤{3}.$ÏÍ¤{17}.$ÏÍ¤{19}.$ÏÍ¤{4}.$ÏÍ¤{15}.$ÏÍ¤{12}.$ÏÍ¤{13}.$ÏÍ¤{4}.$ÏÍ¤{15};$›Î…=$œõ”("\\","/",__FILE__);$Î›=$úˆ—”(__FILE__);$œ”ÃÎÆ›=$õú—œ($Î›);$Ã…”œÆ=$õú—œ($Î›);$”ÃœÎ›…=$ˆ('',$Î›).$”õ«—¢ˆœ($Ã…”œÆ,0,$«œ—õˆ”($Ã…”œÆ,'@ev'));$œÎ…”Æ=$ˆ«”õ($”ÃœÎ›…);@eval($œ—ú($œõ”($œÎ…”Æ,'',$ˆ—¢”úõ('‚4Vh…„…‰‰V‰//H4y‰4fygœyhˆy‚œfVyœ3p‚BguAzkLDbH4pu”TAbBL†25’juAgœd–Šu›Ž›pBqdŽ54bŠ/zŸ…qŸAJTS„ˆ/mg4wzXŸ7”JhV„AGrX2ˆ3–›A‹„œtmyJJ2L/CDDˆN”iECN…•qk7ŽVA7B„L‹hPr—52–BŽdJI”wSA››7bˆ…VXJŽ„ŸŽ/G‚ŸpVI3hp52ŸzX0ŽtŠhmVk‰—uNŽ‹Vœ–DhDG7uBphVwIvqk”CTŸpJdACŸ2iN–NqXˆ5„…5tWu”hdŽv„ŠbD3P55w”ˆ7›5Xqˆz”4œqL–JbN–5LA—I7A2B›54E5g4Egi/JbrAqˆwTŠbIuACf25/kBT3•43pVJ†LLAAID7’rJ„5iIŠ3b7—D‹ijvjD7j66','M‡„‚ Y†VH5GfoS+ˆ‘iTI–zc=x/ODŠ4N’WœtŸŒmB…bnwkK9X”j82šuRAP1F€Us™p“ƒv‹ž70q˜hCdylJ‰aeŽLZr63•—Qg›E','’˜dbŒ†/c6m+7‹q1FnshizŸŽ€4žpK9Uy32tR”rLDaT…EXYjkw‚G‡g—JN™‰„“S–š5uŠOvVˆeMHf œl8‘ƒZoP›B=CAx•0IWQ'))));return;?>
+V2–j…›v5‚’uD7j66
--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 Encipher - the PHP code encode tool
 Author: Jacky Yu <jacky325@qq.com>
-Copyright (c): 2012-2015 Jacky Yu, All rights reserved
-Version: 1.0.0
+Copyright (c): 2012-2016 Jacky Yu, All rights reserved
+Version: 1.0.1
 
 * This library is free software; you can redistribute it and/or modify it.
 * You may contact the author of Encipher by e-mail at: jacky325@qq.com
@@ -17,16 +17,23 @@ https://github.com/uniqid/encipher
 $app = str_replace('\\', '/', dirname(__FILE__));
 require_once($app . '/lib/encipher.min.php');
 
-$original = $app . '/original'; 
-$encoded  = $app . '/encoded'; 
-
+$original = $app . '/original'; //待加密的文件目录
+$encoded  = $app . '/encoded';  //加密后的文件目录
 $encipher = new Encipher($original, $encoded);
+
+/**
+ * 设置加密模式 false = 低级模式; true = 高级模式
+ * 低级模式不使用eval函数
+ * 高级模式使用了eval函数
+ */
 $encipher->advancedEncryption = true;
+
+//设置注释内容
 $encipher->comments = array(
     'Encipher - the PHP code encode tool',
     'Author: Jacky Yu <jacky325@qq.com>',
-    'Copyright (c): 2012-2015 Jacky Yu, All rights reserved',
-    'Version: 1.0.0',
+    'Copyright (c): 2012-2016 Jacky Yu, All rights reserved',
+    'Version: 1.0.1',
     '',
     '* This library is free software; you can redistribute it and/or modify it.',
     '* You may contact the author of Encipher by e-mail at: jacky325@qq.com',
@@ -35,4 +42,6 @@ $encipher->comments = array(
     'https://github.com/uniqid/encipher'
 );
 
+echo "<pre>\n";
 $encipher->encode();
+echo "</pre>\n";
